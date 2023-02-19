af1acb0c22070aa9960349cf477ff0d7aa908e1a
Evgeny R <ryderi700@gmail.com>
2023-02-19 15:03:08 +0300

    added clicable property and fix error

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,17 +2,21 @@ package com.bignerdranch.android.geomain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
-    private lateinit var questionTextView: Button
+    private lateinit var questionTextView: TextView
 
     val questionBank = listOf(
         Questions(R.string.question_australia, true),
@@ -25,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "OnCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -34,15 +39,18 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            checkAnswer(true)
-        }
-        falseButton.setOnClickListener { view: View ->
-            checkAnswer(false)
-        }
-        falseButton.setOnClickListener {
+
             currentIndex = (currentIndex + 1) % questionBank.size
+            checkAnswer(true)
             updateQuestion()
         }
+        falseButton.setOnClickListener { view: View ->
+
+            currentIndex = (currentIndex + 1) % questionBank.size
+            checkAnswer(false)
+            updateQuestion()
+        }
+
         questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
@@ -50,9 +58,36 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart() called")
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        trueButton.isClickable=true
+        falseButton.isClickable=true
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
@@ -60,5 +95,7 @@ class MainActivity : AppCompatActivity() {
         val messageResId = if (userAnswer == correctAnswer) R.string.correct_toast else R.string.incorrect_toast
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+        if (userAnswer == true) falseButton.isClickable=false else trueButton.isClickable=false
     }
+
 }
