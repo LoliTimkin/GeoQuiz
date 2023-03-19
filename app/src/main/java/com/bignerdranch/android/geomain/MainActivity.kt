c3610d4cdb7e2b96a4702450f61f1fe12cf5f791
Evgeny R <ryderi700@gmail.com>
2023-03-19 14:09:00 +0300

    added second activity, intent - part1.

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geomain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,54 +9,76 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
+private const val INDEX_KEY = "index"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    val questionBank = listOf(
-        Questions(R.string.question_australia, true),
-        Questions(R.string.question_oceans, true),
-        Questions(R.string.question_mideast, false),
-        Questions(R.string.question_africa, false)
-    )
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
-    var currentIndex = 0
+    var count: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "OnCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
+
+        val currentIndex = savedInstanceState?.getInt(INDEX_KEY, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
-        trueButton.setOnClickListener { view: View ->
+        updateQuestion()
 
-            currentIndex = (currentIndex + 1) % questionBank.size
+        trueButton.setOnClickListener { view: View ->
+            //currentIndex = (currentIndex + 1) % questionBank.size
             checkAnswer(true)
+            quizViewModel.moveToNext()
             updateQuestion()
         }
         falseButton.setOnClickListener { view: View ->
-
-            currentIndex = (currentIndex + 1) % questionBank.size
+            //currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             checkAnswer(false)
             updateQuestion()
         }
 
+        cheatButton.setOnClickListener{
+            // начало CheatActivity
+            //val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivity(intent)
+        }
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            //currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
-        updateQuestion()
+
+        nextButton.setOnClickListener {
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+        //updateQuestion()
     }
 
     override fun onStart() {
@@ -74,6 +97,11 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onPause() called")
     }
 
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt(INDEX_KEY, quizViewModel.currentIndex)
+    }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
@@ -84,18 +112,28 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy() called")
     }
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        //Log.d(TAG, "Updating question text", Exception())
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
         trueButton.isClickable=true
         falseButton.isClickable=true
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) R.string.correct_toast else R.string.incorrect_toast
+        val messageResId = if (userAnswer == quizViewModel.currentQuestionAnswer) {
+            R.string.correct_toast
+        }
+        else R.string.incorrect_toast
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+        if (quizViewModel.currentIndex <= quizViewModel.questionBank.size && userAnswer == quizViewModel.currentQuestionAnswer) {
+            count++
+        }
         if (userAnswer == true) falseButton.isClickable=false else trueButton.isClickable=false
+        if (quizViewModel.currentIndex == quizViewModel.questionBank.size)
+            Toast.makeText(this, "Количество правильных ответов: $count",Toast.LENGTH_SHORT)
     }
+
+
 
 }
